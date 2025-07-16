26aec8f46a0235a0c07af39b33acbd145d9209c7
Igor Ya <yakovlevigor99@yandex.ru>
2025-07-16 15:10:26 +0300

    done

--- l1/25/25.go
+++ l1/25/25.go
@@ -1,1 +1,31 @@
-package _5
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+Реализовать собственную функцию sleep(duration) аналогично встроенной функции time.Sleep, которая приостанавливает выполнение текущей горутины.
+
+Важно: в отличии от настоящей time.Sleep, ваша функция должна именно блокировать выполнение (например, через таймер или цикл), а не просто вызывать time.Sleep :) — это упражнение.
+
+Можно использовать канал + горутину, или цикл на проверку времени (не лучший способ, но для обучения).
+*/
+
+func Sleep(d time.Duration) {
+	ch := make(chan struct{})
+	go func() {
+		time.AfterFunc(d, func() {
+			close(ch)
+		})
+	}()
+	<-ch
+}
+
+func main() {
+	fmt.Println("started")
+
+	Sleep(1 * time.Second)
+	fmt.Println("ended")
+}
